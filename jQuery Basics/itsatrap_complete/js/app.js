71d55dfe5335684eea32a84917296c8e6b45824a
nebulae12 <nebulae12@gmail.com>
2017-07-20 20:44:34 -0400

    initial commit local repo

---
+++ jQuery Basics/itsatrap_complete/js/app.js
@@ -0,0 +1,6 @@
+//Hide Warning
+// jQuery(".warning").hide();
+//Show Warning Slowly
+// jQuery(".warning").show("show");
+// use jQuery shorthand and method chaining:
+$(".warning").hide().show("slow");
